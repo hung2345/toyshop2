dc3742a1c9ce93aaed03c70f781f4954b1e2bd04
hung2345 <145078386+hung2345@users.noreply.github.com>
2023-10-20 09:10:48 +0700

    Update staff-login.php

--- staff-login.php
+++ staff-login.php
@@ -7,12 +7,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
    
     $dsn = "mysql:host=s29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=lm0ft0r9qtusvm42";
-    $username = "dolspoxwgf3anvkc";
-    $password = "vvvlinl8ngt5rjnp"; 
+    $dbUsername = "dolspoxwgf3anvkc";
+    $dbPassword = "vvvlinl8ngt5rjnp";
+
     
 
     try {
-        $conn = new PDO($dsn, $dbUsername, $dbPassword);
+         $conn = new PDO($dsn, $dbUsername, $dbPassword);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
