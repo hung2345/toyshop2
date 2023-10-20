1591834c677b4a4a77a1224c58e93bce9d87a296
hung2345 <145078386+hung2345@users.noreply.github.com>
2023-10-20 09:13:58 +0700

    Update staff-login.php

--- staff-login.php
+++ staff-login.php
@@ -5,21 +5,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-   
     $dsn = "mysql:host=s29oj5odr85rij2o.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;port=3306;dbname=lm0ft0r9qtusvm42";
     $dbUsername = "dolspoxwgf3anvkc";
-    $dbPassword = "vvvlinl8ngt5rjnp";
-
-    
+    $dbPassword = "vvvlinl8ngt5rjnp"; 
 
     try {
-         $conn = new PDO($dsn, $dbUsername, $dbPassword);
+        $conn = new PDO($dsn, $dbUsername, $dbPassword);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
         echo "Connection failed: " . $e->getMessage();
         exit;
     }
-
 
     $stmt = $conn->prepare("SELECT * FROM staff WHERE email = :email AND password = :password");
     $stmt->bindParam(':email', $email);
@@ -27,91 +23,88 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->execute();
 
     if ($stmt->rowCount() > 0) {
-   
         $_SESSION['user_id'] = $email;
         header("Location: clientforstaff.php");
         exit;
     } else {
-
-        echo "Invalid login information. Please try again..";
+        echo "Invalid login information. Please try again.";
     }
 }
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Staff Login</title>
-  <style>
-    body {
-      font-family: Arial, sans-serif;
-      margin: 0;
-      padding: 0;
-      background-color: #f4f4f4;
-    }
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staff Login</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
 
-    .container {
-      max-width: 400px;
-      margin: 0 auto;
-      padding: 20px;
-      background-color: #fff;
-      box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-      border-radius: 5px;
-      text-align: center;
-    }
+        .container {
+            max-width: 400px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+            text-align: center;
+        }
 
-    h2 {
-      font-size: 24px;
-      margin: 0;
-      padding: 10px;
-    }
+        h2 {
+            font-size: 24px;
+            margin: 0;
+            padding: 10px;
+        }
 
-    .login-form {
-      margin: 20px 0;
-    }
+        .login-form {
+            margin: 20px 0;
+        }
 
-    .form-group {
-      margin: 10px 0;
-    }
+        .form-group {
+            margin: 10px 0;
+        }
 
-    label {
-      display: block;
-    }
+        label {
+            display: block;
+        }
 
-    input {
-      width: 100%;
-      padding: 10px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-    }
+        input {
+            width: 100%;
+            padding: 10px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+        }
 
-    .login-button {
-      background-color: #333;
-      color: #fff;
-      padding: 10px 20px;
-      border: none;
-      border-radius: 5px;
-      cursor: pointer;
-    }
-  </style>
+        .login-button {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+        }
+    </style>
 </head>
 <body>
-  <div class="container">
+<div class="container">
     <h2>Staff Login</h2>
     <form class="login-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-      <div class="form-group">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required>
-      </div>
-      <div class="form-group">
-        <label for="password">Password:</label>
-        <input type="password" name="password" id="password" required>
-      </div>
-      <button type="submit" class="login-button">Login</button>
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password:</label>
+            <input type="password" name="password" id="password" required>
+        </div>
+        <button type="submit" class="login-button">Login</button>
     </form>
-  </div>
+</div>
 </body>
 </html>
